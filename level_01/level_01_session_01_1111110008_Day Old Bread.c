8ae3ee1e55a639f193b2792c1a4dd5fd29a2e6f6
RAJESH D BORATE <rajeshborate08@gmail.com>
2019-03-26 21:12:36 +0530

    Create level_01_session_01_1111110008_Day Old Bread.c

---
+++ level_01/level_01_session_01_1111110008_Day Old Bread.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  float p=185;
+  float dis=p*3/5;
+  float n,a,b;
+  scanf("%f",&n);
+  a=p*n;
+  b=dis*n;
+  
+  printf("Regular Price=%.f",a);
+  printf("\nTotal Discount=%.f",b);
+  printf("\nTotal Amount to be paid=%.f",a-b);
+
+	return 0;
+}
