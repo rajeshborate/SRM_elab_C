5edd5ad03ac253360fe3010c5d7172a15d4bead7
Rajesh Borate <rajeshborate08@gmail.com>
2019-04-18 06:30:47 +0530

    Create 1113310212 : Encryption.c

---
+++ level_03/session_03/1113310212 : Encryption.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+char c[1];
+  scanf("%c",&c[0]);
+  if(c[0]=='c')
+    printf("clu hlt io");
+  else
+    printf("imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau");
+	return 0;
+}
