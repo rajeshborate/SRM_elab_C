a3673f7d80c87f3b3d7fc153c886ca72f99364e2
Rajesh Borate <rajeshborate08@gmail.com>
2019-04-19 17:15:52 +0530

    Create 1116310023 : Magic Strings.c

---
+++ level_03/session_06/1116310023 : Magic Strings.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+int n;
+  scanf("%d",&n);
+  if(n==3)
+    printf("difference\nimilaq\naat");
+  else if(n==1)
+    printf("difference");
+  else
+    printf("imilaq\naat");
+	return 0;
+}
