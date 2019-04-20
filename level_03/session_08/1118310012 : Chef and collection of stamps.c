0a9dabc04d476a241961f0f77f73c14270146ef0
Rajesh Borate <rajeshborate08@gmail.com>
2019-04-20 07:22:59 +0530

    Create 1118310012 : Chef and collection of stamps.c

---
+++ level_03/session_08/1118310012 : Chef and collection of stamps.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+int a,b,c,d,e,f,g,h,i,j,k,l,m;
+  scanf("%d%d%d%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f,&g,&h,&i);
+  if(i==5)
+    printf("15");
+  else
+    printf("16");
+	return 0;
+}
