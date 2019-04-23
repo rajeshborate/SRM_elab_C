fa1e8974ea66a9fb7810c6827284445f41341691
Rajesh Borate <rajeshborate08@gmail.com>
2019-04-23 07:03:57 +0530

    Create 1111210027 : Print the set of series.c

---
+++ level_02/session_01/1111210027 : Print the set of series.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int n,i=1,j=1,k=1,sum=0;
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+    {
+     printf("%d ",k);
+     j=j+1;
+     k=k+j;
+  }
+   // printf("%d ",sum);
+  
+  
+	return 0;
+}
