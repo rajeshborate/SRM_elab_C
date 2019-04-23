ffc6fb9cb333a6a15a1844785c83741a79f9305a
Rajesh Borate <rajeshborate08@gmail.com>
2019-04-23 07:04:14 +0530

    Create 1111210033 : Negative numbers.c

---
+++ level_02/session_01/1111210033 : Negative numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+  int n,i;
+  int arr[20];
+  scanf("%d",&n);
+  for(i=0;i<n;i++){
+  scanf("%d",&arr[i]);
+    
+  }
+  for(i=0;i<n;i++){
+	if(arr[i]<0)
+      printf("%d ",arr[i]);
+  }
+
+	return 0;
+}
