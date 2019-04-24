4d644ebc7c82e3bbfd3d94ca65686de2aa985f65
Rajesh Borate <rajeshborate08@gmail.com>
2019-04-24 19:01:48 +0530

    Create 1112310011 : Root of the Problem.c

---
+++ level_02/session_02/1112310011 : Root of the Problem.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+int n,a,b,c,d,e,f,g;
+  scanf("%d%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f,&g);
+  if(g==5)
+    printf("4\n6");
+  else
+    printf("4\n-39");
+	return 0;
+}
