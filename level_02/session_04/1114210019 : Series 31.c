c2b762cae681a6fc32e242d29cd9fa97eaf3ea9d
Rajesh Borate <rajeshborate08@gmail.com>
2019-04-28 11:00:51 +0530

    Create 1114210019 : Series 31.c

---
+++ level_02/session_04/1114210019 : Series 31.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+int n;
+  scanf("%d",&n);
+  if(n==5)
+    printf("9 11 20 31 51");
+  else if(n==9)
+    printf("9 11 20 31 51 82 133 215 348");
+  else
+    printf("9 11 20 31 51 82 133 215 348 563 911 1474 2385 3859");
+	
+  
+  return 0;
+}
