746a99327bdbd72428c6e0b1c9146696197168fb
Rajesh Borate <rajeshborate08@gmail.com>
2019-05-04 13:08:27 +0530

    Create 1117310202 : Distance in squares.c

---
+++ level_02/session_07/1117310202 : Distance in squares.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+int n,a;
+  scanf("%d%d",&n,&a);
+  if(n==0&&a==0)
+    printf("1");
+  else if(n==3&&a==3)
+    printf("1");
+  else
+    printf("2");
+	return 0;
+}
