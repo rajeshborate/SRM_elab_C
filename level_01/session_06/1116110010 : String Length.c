5e1fdb3f3c9f148b45089fac5c77bd5815d9fe41
Rajesh Borate <rajeshborate08@gmail.com>
2019-05-18 21:54:56 +0530

    Create 1116110010 : String Length.c

---
+++ level_01/session_06/1116110010 : String Length.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+  char c[100];
+  int i,n;
+  for(i=0;i<100;i++){
+  scanf("%s",c);
+  }
+  n=strlen(c);
+  printf("%d",n);
+  
+
+	return 0;
+}
